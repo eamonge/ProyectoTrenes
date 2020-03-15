735babb1d0ddd758504aaaddcc67ea1c5c07bd4e
eamonge <39276293+eamonge@users.noreply.github.com>
2020-03-15 16:14:01 -0600

    Avance 2- Editación Login Usuario
    Se mejoró el archivo con partes mías (Joseph)

--- html/LoginUsuario.php
+++ html/LoginUsuario.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Usuario Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/diseñologin.css">
 </head>
 
 <body>
-    <div>
-        <form action="../includes/login.inc.php" method="POST">
-            <p>Ingrese sus datos para iniciar sesion</p>
-            <p>Ingrese su Usuario <input type="text" name="txtUsuario"></p>
-            <p>Ingrese su clave <input type="password" name="txtClave"></p>
-            <p><button onclick="Login(txtUsuario.value,txtClave.value)">Ingresar</button></p>
-        </form>
-    </div>
-
+    <form action="../includes/login.inc.php" method="POST">
+        <p>Ingrese sus datos para iniciar sesion</p>
+        <p>Ingrese su Usuario <input type="text" name="txtUsuario"></p>
+        <p>Ingrese su clave <input type="password" name="txtClave"></p>
+        <p><button>Ingresar</button></p>
+    </form>
 </body>
-<footer>
-</footer>
 
 </html>
