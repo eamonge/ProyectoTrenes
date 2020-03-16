9ca6b6fe29b9d22a9f69b9d45c10fea7a2cbd053
eamonge <39276293+eamonge@users.noreply.github.com>
2020-03-15 22:23:30 -0600

    Avance 2- Registro de Usuario PHP (BackEnd)

--- html/RegistroUsuario.php
+++ html/RegistroUsuario.php
@@ -7,13 +7,24 @@
 </head>
 
 <body>
-    <form action="" method="POST">
+    <form action="../includes/new.user.inc.php" method="POST">
         <p>Ingrese sus datos para registrarse</p>
-        <p>Ingrese su ID<input type="text" name="txtID"></p>
-        <p>Ingrese su nombre<input type="text" name="txtNombre"></p>
-        <p>Ingrese sus apellidos<input type="text" name="txtApellidos"></p>
-        <p>Ingrese fecha de nacimiento<input type="text" name="txtFDN"></p>
-        <p>Ingrese su correo electronico<input type="text" name="txtCorreo"></p>
+        <p>Ingrese su nombre</p>
+        <input type="text" name="txtNombre">
+        <p>Ingrese su primer apellido</p>
+        <input type="text" name="txtApellido1">
+        <p>Ingrese su segundo apellido</p>
+        <input type="text" name="txtApellido2">
+        <p>Ingrese cédula</p>
+        <input type="text" name="txtCed">
+        <p>Ingrese su correo electronico</p>
+        <input type="text" name="txtCorreo">
+        <p>Ingrese su contraseña</p>
+        <input type="text" name="txtPWD">
+        <p>Reconfirmar contraseña</p>
+        <input type="text" name="txtPWDConf">
+        <p>Ingrese fecha de nacimiento</p>
+        <input type="date" name="txtFDN">
         <p><button>Ingresar</button></p>
     </form>
 </body>
---
+++ includes/new.user.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+include('db.connect.inc.php');
+
+$UsernameInput = $_POST['txtNombre'];
+$PrimApellido = $_POST['txtApellido1'];
+$SegApellido = $_POST['txtApellido2'];
+$CedUsuario = $_POST['txtCed'];
+$CorrUsuario = $_POST['txtCorreo'];
+$CntrsnaUsuario = $_POST['txtPWD'];
+$ConfirmarCntrsna = $_POST['txtPWDConf'];
+$FechaDeNac = $_POST['txtFDN'];
+
+$HashPwd = hash("sha256", $CntrsnaUsuario);
+
+
+
+if ($CntrsnaUsuario == $ConfirmarCntrsna)
+{
+
+    $sql = "INSERT INTO usuarios(Nombre, PrimerApellido, SegundoApellido, Cedula, Correo, Contrasena, FdN, TipoUsuario)
+    VALUES('$UsernameInput', '$PrimApellido', '$SegApellido', '$CedUsuario', '$CorrUsuario', '$HashPwd', '$FechaDeNac', 0);";
+
+    if (!mysqli_query($db, $sql))
+    {
+        echo ("Error description: " . mysqli_error($db));
+    } else
+    {
+        echo '<script>alert("Usuario ingresado exitosamente")</script>';
+        header('Refresh:1;url=/ProyectoTrenes/html/RegistroUsuario.php');
+    }
+
+    // echo "<script> alert('HO'); </script>";
+    // header('Refresh:0;url=/ProyectoTrenes/html/RegistroUsuario.php');
+} else
+{
+    echo '"<script> alert("Las contraseñas deben de coincidir") </script>"';
+    header('Refresh:1;url=/ProyectoTrenes/html/RegistroUsuario.php');
+}
+
+mysqli_close($db);
