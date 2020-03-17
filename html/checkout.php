9519926821c7b6ac7b7599d83306518a8ced608d
Edd2525 <eduardos.z199625@gmail.com>
2020-03-16 21:19:58 -0400

    Modificacion checkout
    Modificacion checkout

--- html/checkout.php
+++ html/checkout.php
@@ -8,54 +8,56 @@
     <title>Checkout</title>
 </head>
 <body>
-    <ul id="menu">
-        <li><input type="checkbox" name="list" id="nivel1-1"><label for="nivel1-1">Credit Card</label>
-        <ul class="interior">
-            <li>
-              <div class="wrapper">
-                <div class="payment">
-                  
-                  <h2>Credit Card</h2>
-                  <div class="form">
-                    
-                    <div class="card space icon-relative">
-                      <label class="label">Card number:</label>
-                      <input type="text" class="input" data-mask="0000 0000 0000 0000" placeholder="Card Number">
-                      <i class="far fa-credit-card"></i>
-                    </div>
-                    <div class="card-grp space">
-                      <div class="card-item icon-relative">
-                        <label class="label">Expiry date:</label>
-                        <input type="text" name="expiry-data" class="input" data-mask="00 / 00"  placeholder="00 / 00">
-                        <i class="far fa-calendar-alt"></i>
-                      </div>
-                      <div class="card-item icon-relative">
-                        <label class="label">CVC:</label>
-                        <input type="text" class="input" data-mask="000" placeholder="000">
-                        <i class="fas fa-lock"></i>
-                      </div>
-                    </div>
-                      
-                    <div class="card space icon-relative">
-                      <label class="label">Card holder:</label>
-                      <input type="text" class="input" placeholder="Coding Market">
-                      <i class="fas fa-user"></i>
-                    </div>
-                    <div class="btn">
-                      Pay
-                    </div> 
-                    
-                  </div>
-                </div>
-              </div>
-            </li>
-        </ul>
-        </li>
-        <li><input type="checkbox" name="list" id="nivel1-2"><label for="nivel1-2">PayPal</label>
-        <ul class="interior">
-            <li>a</li>
-        </ul>
-        </li>
-    </ul>
+
+      <div class="payment">
+        
+        <h2>Tarjeta Credito</h2>
+        <div class="form">
+          
+          <div class="card space icon-relative">
+            <label class="label">Numero de tarjeta:</label>
+            <input type="text" class="input" data-mask="0000 0000 0000 0000" placeholder="Card Number">
+            <i class="far fa-credit-card"></i>
+          </div>
+          <div class="card-grp space">
+            <div class="card-item icon-relative">
+              <label class="label">Fecha de vencimiento:</label>
+              <input type="text" name="expiry-data" class="input" data-mask="00 / 00"  placeholder="00 / 00">
+              <i class="far fa-calendar-alt"></i>
+            </div>
+            <div class="card-item icon-relative">
+              <label class="label">CVC:</label>
+              <input type="text" class="input" data-mask="000" placeholder="000">
+              <i class="fas fa-lock"></i>
+            </div>
+          </div>
+            
+          <div class="card space icon-relative">
+            <label class="label">Titular de la tarjeta:</label>
+            <input type="text" class="input" placeholder="Titular">
+            <i class="fas fa-user"></i>
+          </div>
+          <div class="btn">
+            Pagar
+          </div> 
+        </div>
+</div>
+<div class="preview">
+  <h2>Informacion de compra</h2>
+  <label class="label">Usuario:</label>
+  <input type="text" class="input">
+  <label class="label">Estacion</label>
+  <input type="text" class="input">
+  <label class="label">Tren</label>
+  <input type="text" class="input">
+  <label class="label">Hora de Salida</label>
+  <input type="text" class="input">
+  <label class="label">Hora de Entrada</label>
+  <input type="text" class="input">
+  <label class="label">Monto</label>
+  <input type="text" class="input">
+
+</div>
+        
 </body>
 </html>
